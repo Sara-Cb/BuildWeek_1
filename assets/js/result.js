e3fa0f0cb68680b8d5249cf9ff19ce02cc8511d0
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-02-22 13:10:10 +0100

    d3

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,9 +1,9 @@
 var correctA = 0;
 var wrongA = 0;
 
-if (localStorage.getItem('score')){
+if (localStorage.getItem('score')) {
   correctA = localStorage.getItem('score');
-  wrongA = 10-correctA;
+  wrongA = 10 - correctA;
 }
 
 
@@ -13,13 +13,13 @@ const wrongPercentage = (wrongA / 10) * 100;
 const percentualeGiuste = document.getElementById('percentualeGiuste');
 const domandeCorrette = document.getElementById('domandeCorrette');
 percentualeGiuste.textContent = `${correctPercentage}`;
-domandeCorrette.textContent =  `${correctA} -`;
+domandeCorrette.textContent = `${correctA}/10 `;
 
 const percentualeSbagliate = document.getElementById('percentualeSbagliate');
 const domandeSbagliate = document.getElementById('domandeSbagliate');
 percentualeSbagliate.textContent = `${wrongPercentage}`;
-domandeSbagliate.textContent =  `${wrongA} -`;
-
+domandeSbagliate.textContent = `${wrongA}/10 `;
+/*
 const Grafico = document.getElementById('grafico');
 const risultato1 = document.getElementById('risultato');
 
@@ -32,24 +32,66 @@ function result() {
   }
 
   result();
+*/
+/*
+new Chart(Grafico, {
+  type: 'doughnut',
+  centerText: {
+    display: true,
+    text: 'My label',
+    font: 'Arial',
+    color: '#666666',
 
-
-  new Chart(Grafico, {
-    type: 'doughnut',
-    data: {
-      labels: ['Correct', 'Wrong'],
-      datasets: [{
-        label: 'Your result',
-        borderWidth: 1,
-        cutout: 100,
-        data: [correctA, wrongA],
+  },
+  data: {
+    labels: ['Correct', 'Wrong'],
+    datasets: [{
+      label: 'Your result',
+      borderWidth: 1,
+      cutout: 100,
+      data: [correctA, wrongA],
       backgroundColor: [
         '#00ffff',
         '#d20094',
       ],
       hoverOffset: 4
-      }]
-    },
-  });
-
-  
+    }]
+  },
+});
+*/
+const Grafico = document.getElementById('grafico');
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  width: 800,
+  plugins: {
+    legend: {
+      display: false,
+    }
+  },
+  cutout: '70%',
+  centerText: {
+    display: true,
+    text: 'My label',
+    font: '14px Arial',
+    color: '#666666',
+    sidePadding: 40
+  }
+};
+const chartData = {
+  labels: ['Correct', 'Wrong'],
+  datasets: [
+    {
+      label: 'Your result',
+      borderWidth: 1,
+      data: [correctA, wrongA],
+      backgroundColor: ['#00ffff', '#d20094'],
+      hoverOffset: 4
+    }
+  ]
+};
+const myChart = new Chart(Grafico, {
+  type: 'doughnut',
+  data: chartData,
+  options: options
+});
